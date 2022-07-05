28a2f4a3eb7697f0e408000a12b392a140acd1f7
alexander.rybak <alexander.rybak2020@mail.ru>
2022-07-05 14:14:41 +0300

    third commit!

--- src/main/java/GitMain.java
+++ src/main/java/GitMain.java
@@ -3,5 +3,6 @@ public class GitMain {
     public static void main(String[] args) {
         System.out.println("Hello, Alex!");
         System.out.println("Hello, Roslik!");
+        System.out.println("Hello, President!");
     }
 }
